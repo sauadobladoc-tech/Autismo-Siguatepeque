1f87e41ae6d9c3ae6c29ae3b80b85101baed7e9d
sauadobladoc-tech <saua_dobladoc@unicah.edu>
2025-12-02 01:08:10 -0600

    :wq

--- js/main.js
+++ js/main.js
@@ -26,4 +26,54 @@ document.addEventListener("DOMContentLoaded", ()=>{
             }
         });
     });
+
+    const metodo = document.getElementById("seleccionMetodo");
+    const tarjetacard = document.getElementById("tarjetacard");
+    const transferenciadonacion = document.getElementById("transferenciadonacion");
+    const formDonacion = document.getElementById("formDonacion");
+
+    if (metodo && tarjetacard && transferenciadonacion && formDonacion){
+        metodo.addEventListener("change", () => {
+
+            tarjetacard.style.display = "none";
+            transferenciadonacion.style.display = "none";
+
+            if (metodo.value === "tarjeta"){
+                tarjetacard.style.display = "block";
+            }
+
+            if (metodo.value === "transferencia"){
+                transferenciadonacion.style.display = "block";
+            }
+
+        });
+
+        formDonacion.addEventListener("submit", function(e){
+
+            const monto = document.getElementById("txtMontodonador").value;
+            const metodoPago = metodo.value;
+
+            if (!metodoPago){
+                e.preventDefault();
+                alert("Seleccione un método de pago.");
+                return;
+            }
+
+            if (metodoPago === "paypal"){
+                e.preventDefault();
+                window.open("https://www.paypal.com/donate");
+            }
+
+            if (metodoPago === "tarjeta"){
+                e.preventDefault();
+                alert("Pago con tarjeta pendiente");
+            }
+
+            if (metodoPago === "transferencia"){
+                e.preventDefault();
+                alert("En breve será redirigido a whatsapp.");
+                window.open("https:wa.me/5040000-0000", "_blank");
+            }
+        });
+    }
 });
